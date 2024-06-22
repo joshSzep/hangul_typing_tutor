f1efaf0391c550bde1195102018ad04d1ec54da5
Joshua Szepietowski <joshszep@gmail.com>
2024-06-22 07:46:45 -0700

    Rig up the hello world url

--- hangul_tutor/django_base/urls.py
+++ hangul_tutor/django_base/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
+    path("", include("hangul_tutor.hangul_tutor_app.urls")),
     path("admin/", admin.site.urls),
 ]
