709f4f5f4469ce223c1655f101505f222b833cf3
Joshua Szepietowski <joshszep@gmail.com>
2024-06-22 23:36:47 -0700

    Clean up formatting of views functions.

--- hangul_tutor/hangul_tutor_app/views.py
+++ hangul_tutor/hangul_tutor_app/views.py
@@ -5,14 +5,22 @@ def index(request):
     return HttpResponse("Hello, world. You're at the hangul_tutor_app index.")
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(
+    request,
+    question_id: int,
+):
+    return HttpResponse(f"You're looking at question {question_id}.")
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(
+    request,
+    question_id: int,
+):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(
+    request,
+    question_id: int,
+):
+    return HttpResponse(f"You're voting on question {question_id}.")
