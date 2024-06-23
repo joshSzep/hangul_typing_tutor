6b820155d3433f48cb9770957aebcc6fd76cc2a4
Joshua Szepietowski <joshszep@gmail.com>
2024-06-23 00:25:00 -0700

    Specify question URLs using the url template function.

--- hangul_tutor/hangul_tutor_app/urls.py
+++ hangul_tutor/hangul_tutor_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from hangul_tutor.hangul_tutor_app import views
 
 
+app_name = "hangul_tutor_app"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
