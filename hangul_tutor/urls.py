06353b70d1b526cd08cdf74f676657adff52bac0
Joshua Szepietowski <joshszep@gmail.com>
2024-07-08 09:17:47 -0700

    MIT License will be the license of choice.

--- hangul_tutor/urls.py
+++ hangul_tutor/urls.py
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
         description="API for Hangul Typing Tutor",
         terms_of_service="",
         contact=openapi.Contact(email="joshszep@gmail.com"),
-        license=openapi.License(name="BSD License"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
